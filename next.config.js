c24e5e2e38c5eb024ad35fca90de511add4bad0a
ismael.hini@domissori.fr Hini ismael <titann2.15@outlook.com>
2022-07-11 14:39:44 +0200

    pwa package

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+//   pwa: {
+//     dest: 'public'
+//   }
+// }
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-}
+  pwa: {
+    dest: 'public'
+  }
+})
 
 module.exports = nextConfig
