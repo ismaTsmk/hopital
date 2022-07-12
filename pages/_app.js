f069f42623cc5aa1699e92afe5d5b5abba921900
ismael.hini@domissori.fr Hini ismael <titann2.15@outlook.com>
2022-07-12 09:50:39 +0200

    retry fix beug build navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from '../Components/navbar'
+import Navbar from '../components/navbar'
 
 
 function MyApp({ Component, pageProps }) {
