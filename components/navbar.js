f52d86426780ad14212fa220b408bd3342cbb39e
ismael.hini@domissori.fr Hini ismael <titann2.15@outlook.com>
2022-07-12 11:23:32 +0200

    fix beug and add icons and page etude

--- components/navbar.js
+++ components/navbar.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas'
 import Button from 'react-bootstrap/Button'
-import Link  from 'next/link'
+import Link from 'next/link'
+import { List, HouseDoor } from 'react-bootstrap-icons';
+
 
 
 
@@ -13,9 +15,17 @@ export default function Navbar() {
 
     return (
         <>
-            <Button variant="primary" onClick={handleShow}>
-                Menu
-            </Button>
+            <div className='w-100 position-fixed top-0  end-0 d-flex justify-content-between' >
+                <Link href="/">
+                    <a className="cursor-pointer">
+                        <HouseDoor color="royalblue" size={25} />
+                    </a>
+                </Link>
+                <Button variant="outline-primary" onClick={handleShow}>
+                    <List color="royalblue" size={25} />
+                </Button>
+            </div>
+
 
             <Offcanvas show={show} onHide={handleClose}>
                 <Offcanvas.Header closeButton>
@@ -23,30 +33,34 @@ export default function Navbar() {
                 </Offcanvas.Header>
                 <Offcanvas.Body className="p-0 m-0">
                     <ol className="list-group list-group-numbered">
-                        <Link href="/planning">
+                        <Link href="/planning" onClick={handleClose}>
                             <a>
-                            <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-start border-0">
-                            <div className="ms-2 me-auto">
-                                <div className="fw-bold">Urgence Immediate</div>
-                                Visualiser les urgence non pris en charges. 
-                            </div>
-                            <span className="badge bg-primary rounded-pill">14</span>
-                        </li>
+                                <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-start border-0">
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">Urgence Immediate</div>
+                                        Visualiser les urgence non pris en charges.
+                                    </div>
+                                    <span className="badge bg-primary rounded-pill">14</span>
+                                </li>
                             </a>
 
                         </Link>
+                        <Link href="/etudes" onClick={handleClose}>
+                            <a>
+                                <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-start border-0">
+                                    <div className="ms-2 me-auto">
+                                        <div className="fw-bold">Etude en cours</div>
+                                        Affichez la liste des études mediale en cour.
+                                    </div>
+                                    <span className="badge bg-primary rounded-pill">4</span>
+                                </li>
+                            </a>
 
-                        <li className="list-group-item list-group-item-action list-group-item-action d-flex justify-content-between align-items-start border-0">
-                            <div className="ms-2 me-auto">
-                                <div className="fw-bold">Etude en cours</div>
-                                Affichez la liste des études mediale en cour.
-                            </div>
-                            <span className="badge bg-primary rounded-pill">6</span>
-                        </li>
+                        </Link>
                         <li className="list-group-item d-flex justify-content-between align-items-start border-0">
                             <div className="ms-2 me-auto">
                                 <div className="fw-bold">Mon plannings</div>
-                                Consultez votre plannings et les détail de la journée 
+                                Consultez votre plannings et les détail de la journée
                             </div>
                             <span className="badge bg-primary rounded-pill">2</span>
                         </li>
@@ -57,7 +71,7 @@ export default function Navbar() {
 
                     <div className="p-5 d-flex justify-content-center">
                         <button className='btn btn-outline-secondary'>
-                                Mon compte 
+                            Mon compte
 
                         </button>
                     </div>
